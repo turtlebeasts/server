170feb994009010f6a27422bab262932db1bb4ae
turtlebeasts <52573759+turtlebeasts@users.noreply.github.com>
2022-02-22 23:09:11 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const http = require('http')
+const app = express()
+const server = http.createServer(app)
+const io = require('socket.io')(server, { cors: { origin: '*' } })
+
+app.use(express.static('public'))
+app.use('*', express.static('public/error.html'))
+
+server.listen(3000)
+
+io.on('connection', (socket) => {
+    socket.emit('connected', socket.id)
+    const sockid = socket.id
+    socket.on('message', data => {
+        io.emit('recieve', { data, sockid })
+    })
+})
